c45a714357f39d7fb76da16a5004c824fa3489b8
Lucas Feitosa <lucasfmfeit@gmail.com>
2024-07-12 07:48:16 -0300

    aula 12/07

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-async function getData(){
+async function getData() {
     const response = await fetch("http://localhost:3000/usuarios")
     const data = await response.json()
-    console.log(data)
+    showData(data)
 }
 
 getData()
+
+function showData(data) {
+    const tbody = document.querySelector("tbody")
+    data.forEach((item) => {
+        const tr = document.createElement("tr")
+        
+        const tdNome = document.createElement("td")
+        tdNome.innerHTML = item.nome
+        tr.appendChild(tdNome)
+        
+        const tdEmail = document.createElement("td")
+        tdEmail.innerHTML = item.email
+        tr.appendChild(tdEmail)
+        
+        const tddataNascimento = document.createElement("td")
+        tddataNascimento.innerHTML = item.dataNascimento
+        tr.appendChild(tddataNascimento)
+
+        tbody.appendChild(tr)
+
+    })
+}
