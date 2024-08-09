dff9973cbff4c17081693294854d0bb6395462b8
Lucas Feitosa <lucasfmfeit@gmail.com>
2024-08-09 07:56:33 -0300

    aula 09/08

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ getData()
 
 function showData(data) {
     const tbody = document.querySelector("tbody")
+    tbody.innerHTML=""
     data.forEach((item) => {
         const tr = document.createElement("tr")
 
@@ -23,6 +24,19 @@ function showData(data) {
         tddataNascimento.innerHTML = formatDate(item.dataNascimento)
         tr.appendChild(tddataNascimento)
 
+        const tdAcoes = document.createElement("td")
+        const imgDeletar = document.createElement("img")
+        imgDeletar.src = "images/botao-apagar.png"
+        imgDeletar.onclick = ()=>deleteData(item.id)
+        tdAcoes.appendChild(imgDeletar)
+        
+        const imgEdit = document.createElement("img")
+        imgEdit.src = "images/editar.png"
+        imgEdit.onclick = ()=>handleEdit(item)
+        tdAcoes.appendChild(imgEdit)
+
+        tr.appendChild(tdAcoes)
+
         tbody.appendChild(tr)
 
     })
@@ -37,17 +51,56 @@ function formatDate(data) {
 
 async function sendData(event){
     event.preventDefault()
-    const email = document.getElementById("email").value
-    const nome = document.getElementById("nome").value
-    const data = document.getElementById("data").value
+    const email = document.getElementById("email")
+    const nome = document.getElementById("nome")
+    const data = document.getElementById("data")
     const body = {
-        nome:nome, 
-        email:email,
-        dataNascimento:data
+        nome:nome.value, 
+        email:email.value,
+        dataNascimento:data.value
     }
     const response = await fetch("http://localhost:3000/usuarios",{
         method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
         body: JSON.stringify(body)
+
     })
+    if(response.status===200){
+        nome.value = ""
+        email.value = ""
+        data.value = ""
+        getData()
+    } 
 }
+async function deleteData(id){
+    const response = await fetch(`http://localhost:3000/usuarios/${id}`,{
+        method: "DELETE"
+    })
+    if(response.status===200){
+        getData()
+    }
+}
+
+function handleEdit(data){
+    const email = document.getElementById("email")
+    const nome = document.getElementById("nome")
+    const dataNascimento = document.getElementById("data")
+    email.value = data.email
+    nome.value = data.nome
+    dataNascimento.value = formatDate(data.dataNascimento)
+
+}
+
+async function editData(id){
+    const response = await fetch(`http://localhost:3000/usuarios/${id}`,{
+        method: "PUT"
+    })
+    if(response.status===200){
+        getData()
+    }
+}
+
 
